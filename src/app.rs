4e91d23b3ef82bde1d0eacf13624fd543cebf5de
Arian <arianxdehghani@gmail.com>
2026-03-25 16:04:49 -0700

    Add hardcoded username

--- src/app.rs
+++ src/app.rs
@@ -11,6 +11,7 @@ extern "C" {
 
 #[derive(Serialize, Deserialize)]
 struct MessageArgs<'a> {
+    user: &'a str,
     message: &'a str,
 }
 
@@ -18,6 +19,7 @@ struct MessageArgs<'a> {
 pub fn App() -> impl IntoView {
     let (name, set_name) = signal(String::new());
     let (msg, set_msg) = signal(String::new());
+    const HARDCODED_USER: &str = "Luke Smith";
 
     let update_name = move |ev| {
         let v = event_target_value(&ev);
@@ -32,7 +34,7 @@ pub fn App() -> impl IntoView {
                 return;
             }
 
-            let args = serde_wasm_bindgen::to_value(&MessageArgs { message: &name }).unwrap();
+            let args = serde_wasm_bindgen::to_value(&MessageArgs { user: HARDCODED_USER, message: &name }).unwrap();
             // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
             let new_msg = invoke("send", args).await.as_string().unwrap();
             set_msg.set(new_msg);

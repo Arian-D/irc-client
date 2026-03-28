e02f89995603f7cc0529b205d4cd547658046c3d
Arian <arianxdehghani@gmail.com>
2026-03-28 16:28:01 -0700

    Create type for the commands
    This is meant to be used both for displaying /and/ for parsing when we
    get to it

--- src-tauri/src/irc.rs
+++ src-tauri/src/irc.rs
@@ -23,6 +23,50 @@ struct Message<'a> {
     params: Option<Vec<&'a str>>,
 }
 
+enum Command<'a> {
+    /// Nick message: Set nickname
+    Nick(&'a str),
+    /// USER message: Set username and real name
+    User(&'a str, &'a str),
+    /// QUIT the server with an optional message
+    Quit(Option<&'a str>),
+    // TODO: Implement passwords
+    /// JOIN 1 or more channels.
+    Join(Vec<&'a str>),
+    /// PART message: leave 1 or more channels
+    Part(Vec<&'a str>),
+    /// MODE message: Set the channel or user mode with args
+    Mode(Vec<&'a str>),
+    /// TOPIC message: View or optionally set channel topic
+    Topic(&'a str, Option<&'a str>),
+    /// NAMES: List NICKs, optionally providing channels
+    Names(Vec<&'a str>),
+    /// LISIT channel names
+    List(Vec<&'a str>),
+    /// INVITE user to channel
+    Invite(&'a str, &'a str),
+    /// KICK: Kick user (2) from channel (1) with optional reason (3)
+    Kick(&'a str, &'a str, Option<&'a str>),
+    /// PRVMSG: Send message (2) to one or more receivers (1)
+    PrivMsg(Vec<&'a str>, String)
+    // Commands for later
+    // VERSION
+    // STATS
+    // LINKS
+    // CONNECT
+    // TRACE
+    // ADMIN
+    // INFO
+    // WHO
+    // WHOIS
+    // WHOWAS
+    // PONG
+    // AWAY
+}
+
+
+
+
 impl<'a, T: Read + Write> Client<'a, T> {
     fn send_raw_command(mut self, message: Message<'_>) {
         if let Message {
@@ -59,7 +103,17 @@ impl<'a, T: Read + Write> Client<'a, T> {
             tags: None,
             prefix: None,
             command: "USER",
-            params: Some(vec![nick, "_", "_", name.unwrap_or(nick)])
+            params: Some(vec![nick, "_", "_", name.unwrap_or(nick)]),
+        })
+    }
+
+    fn nick_command(mut self) {
+        let nick = self.nick;
+        self.send_raw_command(Message {
+            tags: None,
+            prefix: None,
+            command: "NICK",
+            params: Some(vec![nick]),
         })
     }
 }
@@ -69,5 +123,5 @@ enum Auth<'a> {
     /// NickServ with Nick and Pass (which may not exist)
     Plain(&'a str, Option<&'a str>),
     /// CertFP authentication. Unsure if this can be used in conjunction with the other, so it might need to be relocated.
-    Cert(Option<&'a str>),
+    Cert(&'a str),
 }

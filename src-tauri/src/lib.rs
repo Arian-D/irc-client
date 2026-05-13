2e84404718a1eda4bd45dd4d3ed70c6b472d94b7
Arian <arianxdehghani@gmail.com>
2026-05-13 01:32:49 -0700

    Switch to a Mutex for AppState

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,7 @@
 use log::{debug, info};
 use std::io::Read;
 use std::net::TcpStream;
+use std::sync::Mutex;
 use std::{collections::HashSet, io::Write};
 use tauri_plugin_store::StoreExt;
 mod irc;
@@ -14,15 +15,18 @@ struct Channel {
     messages: Vec<Message>,
 }
 
-struct AppState<'a> {
-    client: irc::Client<'a, TcpStream>,
+struct InternalAppState<'a> {
+    clients: irc::Client<'a, TcpStream>,
     channels: HashSet<Channel>,
 }
+
+type AppState<'a> = Mutex<InternalAppState<'a>>;
 
 /// Send the message
 #[tauri::command]
 fn send(state: tauri::State<AppState>) -> String {
-    let socket = &state.client.socket;
+    let mut state = state.lock().unwrap();
+    let socket = &state.clients.socket;
     "".to_string()
 }
 
@@ -30,17 +34,16 @@ fn send(state: tauri::State<AppState>) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     // TODO: Move these into the "store"
-    let temp_nick = "uniquenick";
+    let temp_nick = "veryuniquenick";
     let temp_server = "irc.libera.chat:6667";
     // TODO: This has to have error handling
     let tcpstream = TcpStream::connect(temp_server).unwrap();
     tauri::Builder::default()
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
-        .manage(AppState {
+        .manage(InternalAppState {
             channels: HashSet::new(),
-            // TODO: Make this a mutex
-            client: irc::Client {
+            clients: irc::Client {
                 server: temp_server,
                 nick: temp_nick,
                 real_name: None,
